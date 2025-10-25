8eca47a73a4da66f20e8e5f51d93fe40757d3de6
Dam595 <96290770+Dam595@users.noreply.github.com>
2025-10-25 14:03:54 +0700

    fixx

--- Assets/_Game/Scripts/BTVN5.cs
+++ Assets/_Game/Scripts/BTVN5.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class BTVN5 : MonoBehaviour
 {
     [Header("Scale Settings")]
@@ -12,7 +11,7 @@ public class BTVN5 : MonoBehaviour
     [SerializeField] private float returnTime = 0.1f;
 
     [Header("Bounce Settings")]
-    public float bounceHeight = 0.5f;
+    [SerializeField] private float bounceHeight = 0.5f;
     [SerializeField] private float bounceUpTime = 0.15f;
     [SerializeField] private float bounceDownTime = 0.2f;
     [SerializeField] private Vector2 stretchUpScale = new Vector2(0.9f, 1.3f);
@@ -20,7 +19,6 @@ public class BTVN5 : MonoBehaviour
 
     private Vector3 originalScale;
     private Vector3 originalPos;
-    private Coroutine anim;
 
     private void Awake()
     {
@@ -30,33 +28,29 @@ public class BTVN5 : MonoBehaviour
 
     private void Start()
     {
-        if (anim != null)
-        {
-            StopCoroutine(anim);
-        }
-        anim = StartCoroutine(AnimationSequence());
+        // Co lại
+        StartCoroutine(ScaleToTargetScale(shrinkScale, shrinkTime));
+        Invoke(nameof(Expand), shrinkTime);
     }
 
-    IEnumerator AnimationSequence()
+    // Giãn ra
+    private void Expand()
     {
-        // Thu nhỏ
-        yield return ScaleToTargetScale(shrinkScale, shrinkTime);
-
-        // Giãn to
-        yield return ScaleToTargetScale(expandScale, expandTime);
-
-        // Quay về scale ban đầu
-        yield return ScaleToTargetScale(originalScale, returnTime);
-
-        // Nhún nhảy
-        yield return BounceRoutine();
+        StartCoroutine(ScaleToTargetScale(expandScale, expandTime));
+        Invoke(nameof(ReturnScale), expandTime);
     }
 
+    // Về scale ban đầu
+    private void ReturnScale()
+    {
+        StartCoroutine(ScaleToTargetScale(originalScale, returnTime));
+        Invoke(nameof(StartBounceUp), returnTime);
+    }
+    
     IEnumerator ScaleToTargetScale(Vector2 targetScale, float duration)
     {
         Vector2 startScale = transform.localScale;
         float time = 0f;
-        // đếm thời gian
         while (time < duration)
         {
             time += Time.deltaTime;
@@ -65,21 +59,32 @@ public class BTVN5 : MonoBehaviour
             transform.localScale = new Vector3(newScale.x, newScale.y, 1f);
             yield return null;
         }
-
-        transform.localScale = new Vector3(targetScale.x, targetScale.y, 1f);
     }
 
-    IEnumerator BounceRoutine()
+    /// <summary>
+    /// Code nhún nhảy
+    /// </summary>
+    private void StartBounceUp()
     {
-        Vector3 startPos = transform.localPosition;
-        // nhảy lên nè
-        yield return MoveAndScale(startPos, startPos + Vector3.up * bounceHeight, originalScale, stretchUpScale, bounceUpTime);
+        Vector3 startPos = originalPos;
+        Vector3 topPos = originalPos + Vector3.up * bounceHeight;
 
-        // rơi xuống nè
-        yield return MoveAndScale(startPos + Vector3.up * bounceHeight, startPos, stretchUpScale, squashDownScale,bounceDownTime);
+        StartCoroutine(MoveAndScale(startPos, topPos, originalScale, stretchUpScale, bounceUpTime));
+        Invoke(nameof(StartBounceDown), bounceUpTime);
+    }
 
-        // quay lại scale ban đầu nè
-        yield return ScaleToTargetScale(originalScale, 0.5f);
+    private void StartBounceDown()
+    {
+        Vector3 startPos = originalPos;
+        Vector3 topPos = originalPos + Vector3.up * bounceHeight;
+
+        StartCoroutine(MoveAndScale(topPos, startPos, stretchUpScale, squashDownScale, bounceDownTime));
+        Invoke(nameof(ReturnBounceScale), bounceDownTime);
+    }
+
+    private void ReturnBounceScale()
+    {
+        StartCoroutine(ScaleToTargetScale(originalScale, 0.3f));
     }
 
     IEnumerator MoveAndScale(Vector3 fromPos, Vector3 toPos, Vector2 fromScale, Vector2 toScale, float duration)
@@ -91,7 +96,6 @@ public class BTVN5 : MonoBehaviour
             float t = time / duration;
 
             transform.localPosition = Vector3.Lerp(fromPos, toPos, t);
-
             Vector2 newScale = Vector2.Lerp(fromScale, toScale, t);
             transform.localScale = new Vector3(newScale.x, newScale.y, 1f);
 
